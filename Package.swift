29548d27dfe03e4e48c38570a585c27f6704e6e7
H3S Ventures <h3sventures@gmail.com>
2025-06-29 12:02:29 -0400

    update hash

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
         .binaryTarget(
             name: "H3SPDFUtilityWrapper",
             url:
-                "https://github.com/kumarmrsatish/H3SPDFUtilityBinary/releases/download/v1.0.1/H3SPDFUtilityWrapper.xcframework.zip",
-            checksum: "f6ca44cabdfffd25337606a62e9ab6c0a2450f5e6acfdb932e5aa84b2a8bd5e3"
+                "https://github.com/kumarmrsatish/H3SPDFUtilityBinary/releases/download/v1.0.2/H3SPDFUtilityWrapper.xcframework.zip",
+            checksum: "849bb55b198ac2571d1b7be52e92c021e92b4a02eda484c7fe8c96b2f62f050b"
         )
 
     ]
