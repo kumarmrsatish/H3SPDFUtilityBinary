2147ad405e859dda5a236b2c8783daebc07ea343
H3S Ventures <h3sventures@gmail.com>
2025-06-29 11:05:24 -0400

    update package

--- Package.swift
+++ Package.swift
@@ -9,18 +9,18 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "H3SPDFUtilityBinary",
-            targets: ["H3SPDFUtilityBinary"]
+            name: "H3SPDFUtilityWrapper",
+            targets: ["H3SPDFUtilityWrapper"]
         )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
-            name: "H3SPDFUtilityBinary",
+            name: "H3SPDFUtilityWrapper",
             url:
-                "https://github.com/kumarmrsatish/H3SPDFUtilityBinary/releases/download/v1.0.0/H3SPDFUtility.xcframework.zip",
-            checksum: "b93bb9f35efa9babf04012639a4b8ef343deb7190955367a09a620b04cb1c0af"
+                "https://github.com/kumarmrsatish/H3SPDFUtilityBinary/releases/download/v1.0.1/H3SPDFUtilityWrapper.xcframework.zip",
+            checksum: "f6ca44cabdfffd25337606a62e9ab6c0a2450f5e6acfdb932e5aa84b2a8bd5e3"
         )
 
     ]
